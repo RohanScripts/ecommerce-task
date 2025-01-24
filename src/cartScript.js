4540c83afdc54f67023f1aba89939dc69b44eae9
ROHAN SHELAR <shelarrohan1810@gmail.com>
2025-01-24 19:06:17 +0530

    cart page responsive

--- src/cartScript.js
+++ src/cartScript.js
@@ -7,26 +7,27 @@ else{
    cartItems = [] 
 }
 
-console.log(cartItems)
+
 
 let totalAmount = 0
 
+
+
 cartItems.forEach((eachItem)=>{
+
     const itemId = eachItem.id
 
     totalAmount = totalAmount + eachItem.price
     
-    console.log(totalAmount);
     document.getElementById("cartContainer").innerHTML = document.getElementById("cartContainer").innerHTML + `<div
-               id="${itemId}" class=" h-[102px] px-[10px] shadow-[0px_1px_13px_0px_#0000000D] flex justify-center items-center" 
+               id="${itemId}" class=" w-auto h-[102px] px-[10px] shadow-[0px_1px_13px_0px_#0000000D] flex justify-center items-center" 
             >
-              <div class="xl:w-[1091px] lg:w-[1000px] md:w-[600px] flex items-center justify-between">
+              <div class="xl:w-[1091px] lg:w-[850px] md:w-[600px] sm:w-[500px] w-[350px]   flex items-center justify-between">
                 <div class="flex items-center gap-[20px] max-w-[176px]">
                   <div
                     class="w-[54px] h-[54px] relative flex justify-center items-center"
                   >
-                    <img class="w-[50px] h-[39px]" src=${eachItem.image
-                    } />
+                    <img class="w-[50px] h-[39px]" src=${eachItem.image} />
                     <img onclick="deleteItem(${eachItem.id},${itemId})"
                       class="absolute top-0 -left-[5px] w-[18px] h-[18px] cursor-pointer"
                       src="./assets/cancel.png"
@@ -47,11 +48,11 @@ cartItems.forEach((eachItem)=>{
                   <div
                     class="w-[48px] h-[32px] flex items-center justify-between"
                   >
-                    <p class="font-[400] text-[16px] leading-[24px]">01</p>
+                    <p class="font-[400] text-[16px] leading-[24px]">${eachItem.quantity}</p>
                     <div class="flex flex-col">
-                      <img
+                      <img 
                         class="w-[16px] h-[16px] cursor-pointer"
-                        src="./assets/Drop-Up.png"
+                        src="./assets/Drop-Up.png" 
                       />
                       <img
                         class="w-[16px] h-[16px] cursor-pointer"
@@ -93,3 +94,5 @@ const deleteItem = (deleteId, itemId) => {
 const roundedTotal = totalAmount.toFixed(2)
 document.getElementById("subTotal").textContent = roundedTotal
 document.getElementById("totalAmount").textContent = roundedTotal
+
+console.log(cartItems)  
--- src/script.js
+++ src/script.js
@@ -2,10 +2,11 @@ const cartProducts = [
   {
     id: 1,
     title: "Fjallraven Foldsack",
-    price: 109.95,
+    price: 110,
     description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
     category: "men's clothing",
     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    quantity: 1,
     rating: {
       rate: 3.9,
       count: 120
@@ -14,10 +15,11 @@ const cartProducts = [
   {
     id: 2,
     title: "Mens Slim Fit Shirt",
-    price: 22.3,
+    price: 22,
     description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
     category: "men's clothing",
     image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    quantity: 1,
     rating: {
       rate: 4.1,
       count: 259
@@ -26,10 +28,11 @@ const cartProducts = [
   {
     id: 3,
     title: "Mens Cotton Jacket",
-    price: 55.99,
+    price: 56,
     description: "Great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
     category: "men's clothing",
     image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    quantity: 1,
     rating: {
       rate: 4.7,
       count: 500
@@ -38,10 +41,11 @@ const cartProducts = [
   {
     id: 4,
     title: "Mens Slim Fit",
-    price: 15.99,
+    price: 16,
     description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
     category: "men's clothing",
     image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    quantity: 1,
     rating: {
       rate: 2.1,
       count: 430
@@ -54,6 +58,7 @@ const cartProducts = [
     description: "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
     category: "jewelery",
     image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    quantity: 1,
     rating: {
       rate: 4.6,
       count: 400
@@ -66,6 +71,7 @@ const cartProducts = [
     description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
     category: "jewelery",
     image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    quantity: 1,
     rating: {
       rate: 3.9,
       count: 70
@@ -74,10 +80,11 @@ const cartProducts = [
   {
     id: 7,
     title: "Gold Plated Ring",
-    price: 9.99,
+    price: 10,
     description: "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
     category: "jewelery",
     image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+    quantity: 1,
     rating: {
       rate: 3,
       count: 400
@@ -86,10 +93,11 @@ const cartProducts = [
   {
     id: 8,
     title: "Rose Gold Earrings",
-    price: 10.99,
+    price: 11,
     description: "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
     category: "jewelery",
     image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+    quantity: 1,
     rating: {
       rate: 1.9,
       count: 100
@@ -102,6 +110,7 @@ const cartProducts = [
     description: "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
     category: "electronics",
     image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+    quantity: 1,
     rating: {
       rate: 3.3,
       count: 203
@@ -114,6 +123,7 @@ const cartProducts = [
     description: "Easy upgrade for faster boot up, shutdown, application load and response. Boosts burst write performance, making it ideal for typical PC workloads",
     category: "electronics",
     image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+    quantity: 1,
     rating: {
       rate: 2.9,
       count: 470
@@ -126,6 +136,7 @@ const cartProducts = [
     description: "High-speed transfer, perfect for multitasking and high workloads",
     category: "electronics",
     image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+    quantity: 1,
     rating: {
       rate: 4.8,
       count: 319
@@ -140,12 +151,13 @@ const productListing = () => {
   cartProducts.map((eachProduct)=>{
     // create cart div
     const cardDiv = document.createElement("div")
-    cardDiv.className="md:w-[270px] sm:w-[200px] h-[322px] flex flex-col justify-between"
+    cardDiv.className="md:w-[270px] sm:w-[200px] h-[322px] flex flex-col justify-between "
     
 
       // card upper
       const cardUpper = document.createElement("div")
-      cardUpper.className="flex flex-col justify-between "
+      cardUpper.className="flex flex-col justify-between border-[2px] border-black rounded-[4px] overflow-hidden"
+      // cardUpper.style.backgroundColor = "#F5F5F5"
       cardDiv.appendChild(cardUpper)
 
         // dustbin
@@ -214,7 +226,7 @@ const productListing = () => {
         // price
         const productPrice = document.createElement("p")
         productPrice.className="text-[#DB4444] font-[500] text-[16px] leading-[24px]"
-        productPrice.textContent=eachProduct.price
+        productPrice.textContent=`₹${eachProduct.price}` 
         cardLower.appendChild(productPrice)
 
     cardContainer.appendChild(cardDiv)
